d15c6ca2e62d718d6612fc57a078345224c067be
VyPham23 <vy.pham200210@vnuk.edu.vn>
2023-10-03 13:15:17 +0700

    commi

---
+++ public/script.js
@@ -0,0 +1,40 @@
+const encodeBtn = document.getElementById('encodeBtn');
+const decodeBtn = document.getElementById('decodeBtn');
+const messageInput = document.getElementById('message');
+const shiftAmountInput = document.getElementById('shiftAmount');
+const resultText = document.getElementById('result');
+
+encodeBtn.addEventListener('click', () => {
+  const message = messageInput.value;
+  const shiftAmount = parseInt(shiftAmountInput.value, 10);
+  const encodedMessage = caesarCipherEncode(message, shiftAmount);
+  resultText.textContent = 'Encoded Message: ' + encodedMessage;
+});
+
+decodeBtn.addEventListener('click', () => {
+  const message = messageInput.value;
+  const shiftAmount = parseInt(shiftAmountInput.value, 10);
+  const decodedMessage = caesarCipherEncode(message, -shiftAmount);
+  resultText.textContent = 'Decoded Message: ' + decodedMessage;
+});
+
+function caesarCipherEncode(text, shift) {
+    let result = '';
+
+    for (let i = 0; i < text.length; i++) {
+      const char = text[i];
+  
+      if (char.match(/[a-zA-Z]/)) {
+        const isUpperCase = char === char.toUpperCase();
+        const baseCharCode = isUpperCase ? 'A'.charCodeAt(0) : 'a'.charCodeAt(0);
+        const charCode = char.charCodeAt(0);
+        const shiftedCharCode = ((charCode - baseCharCode + shift) % 26 + 26) % 26 + baseCharCode;
+  
+        result += String.fromCharCode(shiftedCharCode);
+      } else {
+        result += char;
+      }
+    }
+  
+    return result;
+}
